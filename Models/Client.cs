ee0a6f18e727836239e77fd1a0d3bc2eaabab365
Trevor Mackin <tismetrm@gmail.com>
2020-01-17 10:18:25 -0800

    Add a Client class in the Models directory Include variable declarations to represent DB columns within the Client class

---
+++ Models/Client.cs
@@ -0,0 +1,11 @@
+namespace HairSalon.Models
+{
+  public class Client
+  {
+    public int ClientId { get; set; }
+    public string Name { get; set; }
+    public int StylistId { get; set; }
+    public virtual Stylist Sylist { get; set; }  
+  }
+  
+}
