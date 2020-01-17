b24bc82f30d526d70f7ebfb66f16f227d6138d9e
Trevor Mackin <tismetrm@gmail.com>
2020-01-17 13:13:47 -0800

    Add Edit methods to the ItemsController Create an Items View that displays a form for the user to enter a new client Add links the Items View Index file back to the Homepage, etc.

--- Controllers/ClientsController.cs
+++ Controllers/ClientsController.cs
@@ -17,24 +17,45 @@ namespace HairSalon.Controllers
 
     public ActionResult Index()
     {
-      // List<Client> model = _db.Clients.Include(clients => clients.Stylist).ToList();
-      // List<Client> model = _db.Clients.ToList();
+      List<Client> model = _db.Clients.Include(clients => clients.Stylist).ToList();
+      return View(model);
+    }
+
+    public ActionResult Create()
+    {
+      ViewBag.StylistId = new SelectList(_db.Stylists, "StylistId", "Name");
       return View();
     }
 
-    // public ActionResult Create()
-    // {
-    //   ViewBag.StylistId = new SelectList(_db.Stylists, "StylistId", "Name");
-    //   return View();
-    // }
+    [HttpPost]
+    public ActionResult Create(Client client)
+    {
+      _db.Clients.Add(client);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
 
-    // [HttpPost]
-    // public ActionResult Create(Client client)
-    // {
-    //   _db.Clients.Add(client);
-    //   _db.SaveChanges();
-    //   return RedirectToAction("Index");
-    // }
+
+    public ActionResult Details(int id)
+    {
+      Client thisClient = _db.Clients.FirstOrDefault(clients => clients.ClientId == id);
+      return View(thisClient);
+    }
+
+    public ActionResult Edit(int id)
+    {
+      var thisClient = _db.Clients.FirstOrDefault(clients => clients.ClientId == id);
+      ViewBad.StylistId = new SelectList(_db.Stylists, "StylistId", "Name");
+      return View(thisClient);
+    }
+
+    [HttpPost]
+    public ActionResult Edit(Client client)
+    {
+      _db.Entry(client).State = EntityState.Modified;
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
 
   }
 }
