386180a902b2ebfd68f83ec352d651a7e517e20a
Trevor Mackin <tismetrm@gmail.com>
2020-01-20 14:11:43 -0800

    Add styling

--- Controllers/ClientsController.cs
+++ Controllers/ClientsController.cs
@@ -42,13 +42,6 @@ namespace HairSalon.Controllers
       return View(thisClient);
     }
 
-    public ActionResult Edit(int id)
-    {
-      var thisClient = _db.Clients.FirstOrDefault(clients => clients.ClientId == id);
-      ViewBag.StylistId = new SelectList(_db.Stylists, "StylistId", "Name");
-      return View(thisClient);
-    }
-
     [HttpPost]
     public ActionResult Edit(Client client)
     {
--- Controllers/StylistsController.cs
+++ Controllers/StylistsController.cs
@@ -13,7 +13,7 @@ namespace HairSalon.Controllers
     {
       _db = db;
     }
-    
+
     public ActionResult Index()
     {
       List<Stylist> model = _db.Stylists.ToList();
@@ -38,20 +38,6 @@ namespace HairSalon.Controllers
       Stylist thisStylist = _db.Stylists.FirstOrDefault(stylist => stylist.StylistId == id);
       thisStylist.Clients = _db.Clients.Where(clients => clients.StylistId == id).ToList();
       return View(thisStylist);
-    }
-
-    public ActionResult Edit(int id)
-    {
-      Stylist thisStylist = _db.Stylists.FirstOrDefault(stylist => stylist.StylistId == id);
-      return View(thisStylist);
-    }
-
-    [HttpPost]
-    public ActionResult Edit(Stylist stylist)
-    {
-      _db.Entry(stylist).State = EntityState.Modified;
-      _db.SaveChanges();
-      return RedirectToAction("Index");
     }
 
     public ActionResult Delete(int id)
